c60852173bf65809a647033fbdb1e5d54028bf62
leon9 <leon9393@gmail.com>
2023-09-15 01:54:56 +0500

    Создание репозитория

---
+++ main/script/descendingOrder/descOrder.js
@@ -0,0 +1,18 @@
+﻿function descendingOrder(n) {
+  let temp = n.toString(),
+    arr = [],
+    result;
+  for (let i = 0; i < temp.length; i++) {
+    arr.push(+temp.charAt(i));
+  }
+  result = arr
+    .sort((a, b) => {
+      return b - a;
+    })
+    .join("");
+  return Number(result);
+}
+
+console.log(descendingOrder(42145));
+console.log(descendingOrder(145263));
+console.log(descendingOrder(123456789));
---
+++ main/script/maskify/maskify.js
@@ -0,0 +1,9 @@
+﻿function maskify(string) {
+  let newString = "";
+  for (let i = 0; i < string.length - 4; i++) {
+    newString += "#";
+  }
+  return newString + string.slice(-4);
+}
+
+console.log(maskify("1"));
