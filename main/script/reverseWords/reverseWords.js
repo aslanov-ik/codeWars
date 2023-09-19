3b94564dd06d079e11dd0fdea66939ff03aa9a00
AslanovIK <aslanov.pir@gmail.com>
2023-09-19 21:53:20 +0500

    Задача "Переверни слова"

---
+++ main/script/reverseWords/reverseWords.js
@@ -0,0 +1,15 @@
+﻿/*
+Напишите функцию, которая принимает строковый параметр, меняет местами каждое слово и буквы в строке.
+Все пробелы в строке должны быть сохранены.
+*/
+
+function reverseWords(str) {
+  let splitString = str.split(" ");
+  let result = [];
+  splitString.forEach((item) => {
+    result.push(item.split("").reverse().join(""));
+  });
+  return result.join(" ");
+}
+
+console.log(reverseWords("This is an example!"));
